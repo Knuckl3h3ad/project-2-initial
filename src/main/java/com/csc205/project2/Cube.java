6eddef8f2a731d876507b4fd6d50005481571a7b
Knuckl3h3ad <knuckl3h3ad@outlook.com>
2022-09-16 14:51:30 -0700

    hello

---
+++ src/main/java/com/csc205/project2/Cube.java
@@ -0,0 +1,48 @@
+package com.csc205.project2;
+
+public class Cube implements ThreeDimensionalShape {
+
+    private double width;
+
+    public Cube() {
+        super();
+        this.width = 0.0;
+    }
+    public Cube(double v) {
+        super();
+        this.width = v;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+}
+
+    public double surfaceArea() {
+        return 6 * (width * width);
+    }
+
+    public double volume() {
+        return width * width * width;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Cube{");
+        sb.append("width=")
+                .append(width);
+        sb.append(", ");
+        sb.append("surface area=")
+                        .append(surfaceArea());
+        sb.append(", ");
+        sb.append("volume=")
+                .append(volume());
+        sb.append('}');
+        return sb.toString();
+    }
+}
+
+
---
+++ src/main/java/com/csc205/project2/Cylinder.java
@@ -0,0 +1,49 @@
+package com.csc205.project2;
+
+public class Cylinder implements ThreeDimensionalShape {
+
+    private double radius;
+    private double height;
+
+    public Cylinder() {
+        super();
+        this.height = 4.0;
+        this.radius = 1.0;
+    }
+    public Cylinder(double height, double radius) {
+        this.radius = radius;
+        this.height = height;
+    }
+    public double getHeight() {return height;}
+    public double getRadius() {return radius;}
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double surfaceArea() {
+        return ((2 * Math.PI * radius * height) + (2 * Math.PI * (radius * radius)));
+    }
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Cube{");
+        sb.append("height=")
+                .append(height);
+        sb.append(", ");
+        sb.append("radius=")
+                .append(radius);
+        sb.append(", ");
+        sb.append("surface area=")
+                .append(surfaceArea());
+        sb.append(", ");
+        sb.append("volume=")
+                .append(volume());
+        sb.append('}');
+        return sb.toString();
+    }
+}
